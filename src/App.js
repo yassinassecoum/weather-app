64ec5e5616f1e3a3506313c540623e4548198603
Assecoum Yassin <yassin.assecoum@hotmail.comgit config --global user.email yassin.assecoum@hotmail.com>
2020-05-19 00:57:19 +0200

    whole application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './weather-app/Weather';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
